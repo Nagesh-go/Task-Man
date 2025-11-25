83fe479a92a5db6eda165d1614cc5d4ddea55863
Nagesh-go <kn734917@gmail.com>
2025-11-25 21:06:45 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import Taskform from './Components/Taskform'
+import Tasklist from './Components/Tasklist'
+import Progresstracker from './Components/Progresstracker'
+
+
+export default function App() {
+  const[tasks, setTasks] = useState([]);
+
+  useEffect(()=>{
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+  });
+
+  const addTask = (task)=>{
+    setTasks([...tasks, task])
+  }
+
+  return (
+    <div>
+      <header>
+        <h1>TaskMan</h1>
+        <p><i>Your friendly Task Manager</i></p>
+      </header>
+      <Taskform addTask = {addTask}/>
+      <Tasklist />
+      <Progresstracker />
+      <button>clear All Tasks</button>
+    </div>
+  )
+}
